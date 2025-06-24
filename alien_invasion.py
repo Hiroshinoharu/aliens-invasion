f8899466e2de72de0e385cd361b39d3a5040ca11
Max Ceban <maxceban2019@gmail.com>
2025-06-24 17:03:24 +0100

    Enhance game functionality by adding ship movement controls, updating settings, and improving fullscreen support

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,10 +12,12 @@ class AlienInvasion:
         """Initialize the game and create game resources."""
         pygame.init()
         # Set up some constants
-        self.clock = pygame.time.Clock() # Sets up a clock for framerates
+        self.clock = pygame.time.Clock() # Sets up a clock to control the frame rate, limiting the game loop to 60 FPS in the run_game method
         self.settings = Settings()
         
-        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height)) # Set the screen size
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN) # Set the screen size
+        self.screen_width = self.screen.get_rect().width
+        self.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")  # Set the window title
         
         # Intialise the ship 
@@ -25,6 +27,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -34,6 +37,12 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            # Move the ship based on user input
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            # Check for key releases
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
     
     def _update_screen(self):
         """Update the screen with the current game state."""
@@ -41,7 +50,33 @@ class AlienInvasion:
         self.ship.blitme()
         # Make the most recently drawn screen visible
         pygame.display.flip()
+        
+    def _check_keydown_events(self,event):
+        """Check for key presses."""
+        if event.key == pygame.K_RIGHT:
+            # Move the ship to the right
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+            
+    def _check_keyup_events(self,event):
+        """Check for key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
 
+# Run the game if this file is executed directly
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
--- ship.py
+++ ship.py
@@ -14,6 +14,7 @@ class Ship:
         """
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
         
         # Load the ship image and get its rect.
         self.image = pygame.image.load('assets/blueship.bmp')
@@ -22,8 +23,34 @@ class Ship:
         # Start a new ship at the bottom centre of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
         
+        # Store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+        
+        # Movement Flags
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+        
     def blitme(self):
         """
         Draw the ship at its current location.
         """
         self.screen.blit(self.image, self.rect)
+    
+    def update(self):
+        """ Update the ship's position based on the movement flags."""
+        # Update the ship's position based on the movement flags not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+        
+        # Update rect object from self.x and self.y.
+        self.rect.x = self.x
+        self.rect.y = self.y
